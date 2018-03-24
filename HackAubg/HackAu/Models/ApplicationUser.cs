a90efb43bd39d7ab884f34a1d58a4c406442b886
Unknown <tihomirkirilov96@gmail.com>
2018-03-24 22:32:46 +0200

    EntityConfing , appdbcontext

--- HackAubg/HackAu/Models/ApplicationUser.cs
+++ HackAubg/HackAu/Models/ApplicationUser.cs
@@ -9,5 +9,6 @@ namespace HackAu.Models
     // Add profile data for application users by adding properties to the ApplicationUser class
     public class ApplicationUser : IdentityUser
     {
+        public ICollection<Course> Courses => new List<Course>();
     }
 }
