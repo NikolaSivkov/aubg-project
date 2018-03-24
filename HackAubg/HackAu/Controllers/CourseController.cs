00cc90a6256e39b1f33bad4b79e32f16f7eae341
Unknown <tihomirkirilov96@gmail.com>
2018-03-24 21:48:34 +0200

    Fixed id bug , added course controller

---
+++ HackAubg/HackAu/Controllers/CourseController.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using HackAu.Data;
+using HackAu.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+
+namespace HackAu.Controllers
+{
+    [Authorize]
+    public class CourseController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        //,AspNetUserManager<ApplicationUser> userManager
+        public CourseController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            //_userManager = userManager;
+            _context = context;
+            _userManager = userManager;
+        }
+
+        // GET: Course
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Courses.Include(c => c.Teacher);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Course/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var course = await _context.Courses
+                .Include(c => c.Teacher)
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            return View(course);
+        }
+
+        // GET: Course/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Course/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Subject")] Course course)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = await _userManager.GetUserAsync(User);
+                course.Teacher = user;
+                course.TeacherId = user.Id;
+                _context.Add(course);
+                
+                await _context.SaveChangesAsync();
+                 return RedirectToAction(nameof(Index));
+            }
+            return View(course);
+        }
+
+        // GET: Course/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var course = await _context.Courses.SingleOrDefaultAsync(m => m.Id == id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+            ViewData["TeacherId"] = id;
+            return View(course);
+        }
+
+        // POST: Course/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Subject,TeacherId")] Course course)
+        {
+            if (id != course.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(course);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CourseExists(course.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["TeacherId"] = course;
+            return View(course);
+        }
+
+        // GET: Course/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var course = await _context.Courses
+                .Include(c => c.Teacher)
+                .SingleOrDefaultAsync(m => m.Id == id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            return View(course);
+        }
+
+        // POST: Course/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var course = await _context.Courses.SingleOrDefaultAsync(m => m.Id == id);
+            _context.Courses.Remove(course);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CourseExists(int id)
+        {
+            return _context.Courses.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ HackAubg/HackAu/Data/Migrations/20180324154916_inits.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace HackAu.Data.Migrations
+{
+    public partial class inits : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Courses_AspNetUsers_TeacherId",
+                table: "Courses");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Subject",
+                table: "Courses",
+                nullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Courses_AspNetUsers_TeacherId",
+                table: "Courses",
+                column: "TeacherId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Courses_AspNetUsers_TeacherId",
+                table: "Courses");
+
+            migrationBuilder.DropColumn(
+                name: "Subject",
+                table: "Courses");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Courses_AspNetUsers_TeacherId",
+                table: "Courses",
+                column: "TeacherId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
