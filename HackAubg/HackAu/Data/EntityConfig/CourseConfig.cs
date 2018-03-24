9442445d57f186f5b9ca55d2e525f8e4f5429dd4
Unknown <tihomirkirilov96@gmail.com>
2018-03-24 17:25:14 +0200

    Adding course, students and teachers
    Fixed Get Started button

---
+++ HackAubg/HackAu/Data/EntityConfig/CourseConfig.cs
@@ -0,0 +1,22 @@
+﻿using HackAu.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HackAu.Data.EntityConfig
+{
+    public class CourseConfig : IEntityTypeConfiguration<Course>
+    {
+        public void Configure(EntityTypeBuilder<Course> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.HasOne(x => x.Teacher)
+                .WithMany(x => x.Courses)
+               .HasForeignKey(x => x.TeacherId);
+           
+        }
+    }
+}
---
+++ HackAubg/HackAu/Models/Course.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HackAu.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string TeacherId { get; set; }
+        public ApplicationUser Teacher { get; set; }
+        public ICollection<ApplicationUser> Students => new List<ApplicationUser>();
+    }
+}
